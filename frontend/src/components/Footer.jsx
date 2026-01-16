965f399d5b0c6aca06256022bd4b30cf9fcfaf0e
MohdAyaanSiddiqui <muhammadxayaan007@gmail.com>
2026-01-16 23:34:14 +0530

    Initial Updates In Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,38 +1,43 @@
 import React from 'react'
 
-const Footer = () => {
+const FooterTwo = () => {
   return (
-    <div className="flex flex-col items-center text-slate-800 mt-20">
-    <h2 className="text-3xl font-medium mb-10">
-        Happy
-        <span className="text-indigo-500 font-bold">
-            Clients
-        </span>
-    </h2>
-    <div className="flex items-center -space-x-3">
-        <div className="relative group flex flex-col items-center">
-            <p className="opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 transition duration-300 mb-3 px-2 py-1 text-sm font-medium">Michael</p>
-            <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200" alt="image"
-                className="w-20 h-20 rounded-full border-4 border-white hover:-translate-y-1 transition hover:scale-110"/>
+    <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
+    <div className="max-w-80 bg-black text-white rounded-2xl">
+        <div className="relative -mt-px overflow-hidden rounded-2xl">
+            <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=600" alt="" className="h-[270px] w-full rounded-2xl hover:scale-105 transition-all duration-300 object-cover object-top"/>
+            <div className="absolute bottom-0 z-10 h-60 w-full bg-gradient-to-t pointer-events-none from-black to-transparent"></div>
         </div>
-        <div className="relative group flex flex-col items-center">
-            <p className="opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 transition duration-300 mb-3 px-2 py-1 text-sm font-medium">James</p>
-            <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200" alt="image"
-                className="w-20 h-20 rounded-full border-4 border-white hover:-translate-y-1 transition hover:scale-110"/>
+        <div className="px-4 pb-4">
+            <p className="font-medium border-b border-gray-600 pb-5">“Radiant made undercutting all of our competitors an absolute breeze.”</p>
+            <p className="mt-4">— John Doe</p>
+            <p className="text-sm font-medium bg-gradient-to-r from-[#8B5CF6] via-[#E0724A] to-[#9938CA] text-transparent bg-clip-text">Content Marketing</p>
         </div>
-        <div className="relative group flex flex-col items-center">
-            <p className="opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 transition duration-300 mb-3 px-2 py-1 text-sm font-medium">Emily</p>
-            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200&h=200&auto=format&fit=crop"
-                alt="image" className="w-20 h-20 rounded-full border-4 border-white hover:-translate-y-1 transition hover:scale-110"/>
+    </div>
+    <div className="max-w-80 bg-black text-white rounded-2xl">
+        <div className="relative -mt-px overflow-hidden rounded-2xl">
+            <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=600" alt="" className="h-[270px] w-full rounded-2xl hover:scale-105 transition-all duration-300 object-cover object-top"/>
+            <div className="absolute bottom-0 z-10 h-60 w-full bg-gradient-to-t pointer-events-none from-black to-transparent"></div>
         </div>
-        <div className="relative group flex flex-col items-center">
-            <p className="opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 transition duration-300 mb-3 px-2 py-1 text-sm font-medium">John</p>
-            <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="image"
-                className="w-20 h-20 rounded-full border-4 border-white hover:-translate-y-1 transition hover:scale-110"/>
+        <div className="px-4 pb-4">
+            <p className="font-medium border-b border-gray-600 pb-5">“Radiant made undercutting all of our competitors an absolute breeze.”</p>
+            <p className="mt-4">— John Doe</p>
+            <p className="text-sm font-medium bg-gradient-to-r from-[#8B5CF6] via-[#E0724A] to-[#9938CA] text-transparent bg-clip-text">Content Marketing</p>
+        </div>
+    </div>
+    <div className="max-w-80 bg-black text-white rounded-2xl">
+        <div className="relative -mt-px overflow-hidden rounded-2xl">
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=600&h=600&auto=format&fit=crop" alt="" className="h-[270px] w-full rounded-2xl hover:scale-105 transition-all duration-300 object-cover object-top"/>
+            <div className="absolute bottom-0 z-10 h-60 w-full bg-gradient-to-t pointer-events-none from-black to-transparent"></div>
+        </div>
+        <div className="px-4 pb-4">
+            <p className="font-medium border-b border-gray-600 pb-5">“Radiant made undercutting all of our competitors an absolute breeze.”</p>
+            <p className="mt-4">— John Doe</p>
+            <p className="text-sm font-medium bg-gradient-to-r from-[#8B5CF6] via-[#E0724A] to-[#9938CA] text-transparent bg-clip-text">Content Marketing</p>
         </div>
     </div>
 </div>
   )
 }
 
-export default Footer
+export default FooterTwo
