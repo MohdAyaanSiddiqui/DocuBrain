62593d1b93f42d0274426e1f30472b03c50c3a3b
MohdAyaanSiddiqui <muhammadxayaan007@gmail.com>
2026-01-16 22:04:41 +0530

    Initial Update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="flex items-center border mx-4 max-md:w-full max-md:justify-between border-slate-700 px-6 py-4 rounded-full text-white text-sm">
+        <nav className="flex items-center border mx-4 max-md:w-full max-md:justify-between border-slate-800 px-6 py-4 rounded-full text-white text-sm">
             <a>
                 <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="4.706" cy="16" r="4.706" fill="#D9D9D9" />
@@ -14,43 +14,37 @@ const Navbar = () => {
             <div className="hidden md:flex items-center gap-6 ml-7">
                 <a href="#" className="relative overflow-hidden h-6 group">
                     <span className="block group-hover:-translate-y-full transition-transform duration-300">Products</span>
-                    <span
-                        className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Products</span>
+                    <span className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Products</span>
                 </a>
                 <a href="#" className="relative overflow-hidden h-6 group">
                     <span className="block group-hover:-translate-y-full transition-transform duration-300">Stories</span>
-                    <span
-                        className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Stories</span>
+                    <span className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Stories</span>
                 </a>
                 <a href="#" className="relative overflow-hidden h-6 group">
                     <span className="block group-hover:-translate-y-full transition-transform duration-300">Pricing</span>
-                    <span
-                        className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Pricing</span>
+                    <span className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Pricing</span>
                 </a>
                 <a href="#" className="relative overflow-hidden h-6 group">
                     <span className="block group-hover:-translate-y-full transition-transform duration-300">Docs</span>
-                    <span
-                        className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Docs</span>
+                    <span className="block absolute top-full left-0 group-hover:translate-y-[-100%] transition-transform duration-300">Docs</span>
                 </a>
             </div>
 
             <div className="hidden ml-14 md:flex items-center gap-4">
-                <button
-                    className="border border-slate-600 hover:bg-slate-800 px-4 py-2 rounded-full text-sm font-medium transition">
+                <button className="border border-slate-600 hover:bg-slate-800 px-4 py-2 rounded-full text-sm font-medium transition">
                     Contact
                 </button>
-                <button
-                    className="bg-white hover:shadow-[0px_0px_30px_14px] shadow-[0px_0px_30px_7px] hover:shadow-white/50 shadow-white/50 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-slate-100 transition duration-300">
+                <button className="bg-white hover:shadow-[0px_0px_30px_14px] shadow-[0px_0px_30px_7px] hover:shadow-white/50 shadow-white/50 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-slate-100 transition duration-300">
                     Get Started
                 </button>
             </div>
-            <button id="menuToggle" class="md:hidden text-gray-600">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
-                    stroke-linecap="round" stroke-linejoin="round">
+            <button id="menuToggle" className="md:hidden text-gray-600">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"
+                    strokeLinecap="round" strokeLinejoin="round">
                     <path d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </button>
-            <div id="mobileMenu" class="absolute hidden top-48 text-base left-0 bg-black w-full flex-col items-center gap-4">
+            <div id="mobileMenu" className="absolute hidden top-48 text-base left-0 bg-black w-full flex-col items-center gap-4">
                 <a className="hover:text-indigo-600" href="#">
                     Products
                 </a>
@@ -63,12 +57,10 @@ const Navbar = () => {
                 <a className="hover:text-indigo-600" href="#">
                     Docs
                 </a>
-                <button
-                    className="border border-slate-600 hover:bg-slate-800 px-4 py-2 rounded-full text-sm font-medium transition">
+                <button className="border border-slate-600 hover:bg-slate-800 px-4 py-2 rounded-full text-sm font-medium transition">
                     Contact
                 </button>
-                <button
-                    className="bg-white hover:shadow-[0px_0px_30px_14px] shadow-[0px_0px_30px_7px] hover:shadow-white/50 shadow-white/50 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-slate-100 transition duration-300">
+                <button className="bg-white hover:shadow-[0px_0px_30px_14px] shadow-[0px_0px_30px_7px] hover:shadow-white/50 shadow-white/50 text-black px-4 py-2 rounded-full text-sm font-medium hover:bg-slate-100 transition duration-300">
                     Get Started
                 </button>
             </div>
