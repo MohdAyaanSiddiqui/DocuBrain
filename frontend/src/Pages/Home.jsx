34c2b9a80cb8ede120da8620705c64736419b3d8
MohdAyaanSiddiqui <muhammadxayaan007@gmail.com>
2026-01-16 23:26:53 +0530

    Initial Update In Frontend

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
-
+import Footer from '../components/Footer'
+import FooterTwo from '../components/FooterTwo'
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Footer/>
+    <FooterTwo/>
     </>
   )
 }
